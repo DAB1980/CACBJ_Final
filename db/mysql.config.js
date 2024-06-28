7bb9f3e61a9c0759a7438d6ccbfe3ac3c4eea18c
DAB1980 <davidbaffoni80@gmail.com>
2024-06-28 00:05:35 -0300

    update

--- db/mysql.config.js
+++ db/mysql.config.js
@@ -1,7 +1,22 @@
+// export const config = {
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '',
+//     database: 'cac_bj_db'
+// }
+
 export const config = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'cac_bj_db'
+    host: process.env.HOST_DB,
+    port: process.env.HOST_PORT,
+    user: process.env.USER_DB,
+    password: process.env.PASS_SB,
+    database: process.env.DB
 }
+
+// Host: sql10.freesqldatabase.com
+// Database name: sql10716683
+// Database user: sql10716683
+// Database password: D3TXDXgu6c
+// Port number: 3306
+
