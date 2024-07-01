faf9255fa60d35c2da72096f371b4b788beb90fe
DAB1980 <davidbaffoni80@gmail.com>
2024-07-01 14:59:15 -0300

    actualizo script de BD para WEB

--- db/mysql.config.js
+++ db/mysql.config.js
@@ -1,18 +1,18 @@
-export const config = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'cac_bj_db'
-}
-
 // export const config = {
-//     host: process.env.HOST_DB,
-//     port: process.env.HOST_PORT,
-//     user: process.env.USER_DB,
-//     password: process.env.PASS_SB,
-//     database: process.env.DB
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '',
+//     database: 'cac_bj_db'
 // }
+
+export const config = {
+    host: process.env.HOST_DB,
+    port: process.env.HOST_PORT,
+    user: process.env.USER_DB,
+    password: process.env.PASS_SB,
+    database: process.env.DB
+}
 
 // Host: sql10.freesqldatabase.com
 // Database name: sql10716683
